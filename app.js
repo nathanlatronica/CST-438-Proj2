92597e7643547cd80fdfb5613cd38af21dfaaa29
nathanlatronica <nlatronica@csumb.edu>
2020-10-08 14:08:39 -0700

    home page

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/", function(req, res){
+   res.render("home");
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
