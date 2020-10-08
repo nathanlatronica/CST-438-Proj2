b4f628132c517b215cdd74e61e89a63bc3ce853e
nathanlatronica <nlatronica@csumb.edu>
2020-10-08 14:14:06 -0700

    home page

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express();
 const port = 8000;
 
+app.set("view engine", "ejs");
+app.use(express.static("public")); //folder for images, css, js
+app.use('/public', express.static('public'));
+app.use(express.urlencoded()); //use to parse data sent using the POST method
+
+
 app.get("/", function(req, res){
    res.render("home");
 });
