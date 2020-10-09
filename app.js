491041a5c8fcefeedc39b545bc1a60eaf1affe3b
bsuttonwills <38510991+bsuttonwills@users.noreply.github.com>
2020-10-09 15:17:59 -0700

    Login, sign up, and home screens

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const mysql   = require("mysql");
+const mysql   = require("mysql");
 // const sha256  = require("sha256");
 const session = require('express-session');
 const app = express();
@@ -21,309 +21,42 @@ app.get("/", function(req, res){
    res.render("index");
 });
 
-// app.get("/admin", async function(req, res){
-    
-//    console.log("authenticated: ", req.session.authenticated);    
-   
-//    if (req.session.authenticated) { //if user hasn't authenticated, sending them to login screen
-       
-//      let authorList = await getAuthorList();  
-//        //console.log(authorList);
-//        res.render("admin", {"authorList":authorList});  
-       
-//    }  else { 
-    
-//        res.render("login"); 
-   
-//    }
-// });
+app.get("/login", function(req, res){
+   res.render("login");
+});
 
-// app.post("/loginProcess", function(req, res) {
-    
-//     if ( req.body.username == "admin" && sha256(req.body.password) == "2bb80d537b1da3e38bd30361aa855686bde0eacd7162fef6a25fe97bf527a25b") {
-//        req.session.authenticated = true;
-//        res.send({"loginSuccess":true});
-//     } else {
-//        res.send(false);
-//     }
-
-    
-// });
-
-// app.get("/logout", function(req, res) {
-//     req.session.destroy();
-//     res.redirect("/");
-// });
-
-// app.get("/addAuthor", function(req, res){
-//     if (req.session.authenticated) { //if user hasn't authenticated, sending them to login screen
- 
-//        res.render("newAuthor"); 
-       
-//    }  else { 
-    
-//        res.render("login"); 
-   
-//    }
-  
-// });
-
-// app.post("/addAuthor", async function(req, res){
-//   //res.render("newAuthor");
-//     if (req.session.authenticated) {
-//         let rows = await insertAuthor(req.body);
-//         console.log(rows);
-//         //res.send("First name: " + req.body.firstName); //When using the POST method, the form info is stored in req.body
-//         let message = "Author WAS NOT added to the database!";
-//         if (rows.affectedRows > 0) {
-//           message= "Author successfully added!";
-//         }
-//         res.render("newAuthor", {"message":message});
-//     } else { 
-    
-//         res.render("login"); 
-   
-//    }
-    
-    
-// });
-
-// app.get("/updateAuthor", async function(req, res){
-//     if (req.session.authenticated) { 
- 
-//         let authorInfo = await getAuthorInfo(req.query.authorId);    
-//         //console.log(authorInfo);
-//         res.render("updateAuthor", {"authorInfo":authorInfo}); 
-       
-//    }  else { 
-    
-//        res.render("login"); 
-   
-//    }
-    
-// });
-
-// app.post("/updateAuthor", async function(req, res){
-//     if (req.session.authenticated) { 
-//         let rows = await updateAuthor(req.body);
-        
-//         let authorInfo = req.body;
-//         console.log(rows);
-//         //res.send("First name: " + req.body.firstName); //When using the POST method, the form info is stored in req.body
-//         let message = "Author WAS NOT updated!";
-//         if (rows.affectedRows > 0) {
-//           message= "Author successfully updated!";
-//         }
-//       res.render("updateAuthor", {"message":message, "authorInfo":authorInfo});
-//     } else { 
-    
-//        res.render("login"); 
-   
-//    } 
-    
-// });
-
-// app.get("/deleteAuthor", async function(req, res){
-//     if (req.session.authenticated) { //if user hasn't authenticated, sending them to login screen
- 
-//         let rows = await deleteAuthor(req.query.authorId);
-//         console.log(rows);
-//         //res.send("First name: " + req.body.firstName); //When using the POST method, the form info is stored in req.body
-//         let message = "Author WAS NOT deleted!";
-//         if (rows.affectedRows > 0) {
-//             message= "Author successfully deleted!";
-//         }    
-        
-//         let authorList = await getAuthorList();  
-//         //console.log(authorList);
-//         res.render("admin", {"authorList":authorList});
-       
-//     }  else { 
-    
-//         res.render("login"); 
-   
-//     }
-    
-// });
-
-// app.get("/dbTest", function(req, res){
-
-//     let conn = dbConnection();
-    
-//     conn.connect(function(err) {
-//        if (err) throw err;
-//        console.log("Connected!");
-    
-//        let sql = "SELECT * FROM q_author WHERE sex = 'F'";
-    
-//        conn.query(sql, function (err, rows, fields) {
-//           if (err) throw err;
-//           conn.end();
-//           res.send(rows);
-//        });
-    
-//     });
-
-// });//dbTest
-
-// //functions
-
-// function insertAuthor(body){
-   
-//    let conn = dbConnection();
-    
-//     return new Promise(function(resolve, reject){
-//         conn.connect(function(err) {
-//            if (err) throw err;
-//            console.log("Connected!");
-        
-//            let sql = `INSERT INTO q_author
-//                         (firstName, lastName, sex)
-//                          VALUES (?,?,?)`;
-        
-//            let params = [body.firstName, body.lastName, body.gender];
-        
-//            conn.query(sql, params, function (err, rows, fields) {
-//               if (err) throw err;
-//               //res.send(rows);
-//               conn.end();
-//               resolve(rows);
-//            });
-        
-//         });//connect
-//     });//promise 
-// }
-
-// function updateAuthor(body){
-   
-//    let conn = dbConnection();
-    
-//     return new Promise(function(resolve, reject){
-//         conn.connect(function(err) {
-//            if (err) throw err;
-//            console.log("Connected!");
-        
-//            let sql = `UPDATE q_author
-//                       SET firstName = ?, 
-//                           lastName  = ?, 
-//                                 sex = ?
-//                      WHERE authorId = ?`;
-        
-//            let params = [body.firstName, body.lastName, body.gender, body.authorId];
-        
-//            console.log(sql);
-           
-//            conn.query(sql, params, function (err, rows, fields) {
-//               if (err) throw err;
-//               //res.send(rows);
-//               conn.end();
-//               resolve(rows);
-//            });
-        
-//         });//connect
-//     });//promise 
-// }
+app.get("/signup", function(req, res){
+   res.render("signup");
+});
 
 
+var PORT = process.env.PORT || 8000;
+if (process.env.JAWSDB_URL) {
+   var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+   var connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "password",
+      database: "users_db"
+   })
+}
 
-// function deleteAuthor(authorId){
-   
-//    let conn = dbConnection();
-    
-//     return new Promise(function(resolve, reject){
-//         conn.connect(function(err) {
-//            if (err) throw err;
-//            console.log("Connected!");
-        
-//            let sql = `DELETE FROM q_author
-//                       WHERE authorId = ?`;
-        
-//            conn.query(sql, [authorId], function (err, rows, fields) {
-//               if (err) throw err;
-//               //res.send(rows);
-//               conn.end();
-//               resolve(rows);
-//            });
-        
-//         });//connect
-//     });//promise 
-// }
-// function getAuthorInfo(authorId){
-   
-//    let conn = dbConnection();
-    
-//     return new Promise(function(resolve, reject){
-//         conn.connect(function(err) {
-//            if (err) throw err;
-//            console.log("Connected!");
-        
-//            let sql = `SELECT *
-//                       FROM q_author
-//                       WHERE authorId = ?`;
-        
-//            conn.query(sql, [authorId], function (err, rows, fields) {
-//               if (err) throw err;
-//               //res.send(rows);
-//               conn.end();
-//               resolve(rows[0]); //Query returns only ONE record
-//            });
-        
-//         });//connect
-//     });//promise 
-// }
+function dbConnection(){
 
-// function getAuthorList(){
-   
-//    let conn = dbConnection();
-    
-//     return new Promise(function(resolve, reject){
-//         conn.connect(function(err) {
-//            if (err) throw err;
-//            console.log("Connected!");
-        
-//            let sql = `SELECT authorId, firstName, lastName 
-//                         FROM q_author
-//                         ORDER BY lastName`;
-        
-//            conn.query(sql, function (err, rows, fields) {
-//               if (err) throw err;
-//               //res.send(rows);
-//               conn.end();
-//               resolve(rows);
-//            });
-        
-//         });//connect
-//     });//promise 
-// }
+   let conn = mysql.createConnection({
+                 host: "localhost",
+                 user: "root",
+             password: "password",
+             database: "users_db"
+       }); //createConnection
 
-// // function dbConnection(){
+return conn;
 
-// //   let conn = mysql.createConnection({
-// //                  host: "cst336db.space",
-// //                  user: "cst336_dbUser",
-// //              password: "secret19",
-// //              database: "cst336_db"
-// //       }); //createConnection
-
-// // return conn;
-
-// // }
-
-// function dbConnection(){
-
-//    let conn = mysql.createConnection({
-//                  host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//                  user: "kd892qz9jpxwqtxq",
-//              password: "kjjtymmpwpomk5ie",
-//              database: "yjwepa0cf8l7lsku"
-//        }); //createConnection
-
-// return conn;
-
-// }
+}
 
 
 //starting server
-app.listen(process.env.PORT || 3000, function(){
+app.listen(PORT, function(){
    console.log("Express server is running...");
 });
